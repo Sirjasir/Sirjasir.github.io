32122b04374108ec9978234e7d3f411f9c769c99
Sirjasir <154694013+Sirjasir@users.noreply.github.com>
2025-11-01 12:02:58 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBW7F4lrLQG8-OqLaE9hpXI9QxdTWaThXo",
+  authDomain: "ffnfeedback.firebaseapp.com",
+  projectId: "ffnfeedback",
+  storageBucket: "ffnfeedback.appspot.com",
+  messagingSenderId: "469095529693",
+  appId: "1:469095529693:web:b8f8a1c24593b67c9e6f34",
+  measurementId: "G-CDDM310YEW"
+};
